4fb418c53b245760fd63f8aaab04d64e4dc2c237
Salman Al Majali <undermod007@gmail.com>
2026-01-28 21:50:25 +0700

    add tests for Joke class to validate joke retrieval by locale

---
+++ tests/JokeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Http;
+use SalmanAlmajali\JokesWidget\Joke;
+
+it('returns an english joke from API when locale is en', function () {
+    app()->setLocale('en');
+    
+    Http::fake([
+        'icanhazdadjoke.com/*' => Http::response(['joke' => 'Funny API Joke'], 200),
+    ]);
+
+    $joke = Joke::tellAJoke();
+    expect($joke)->toContain('Funny API Joke');
+});
+
+it('returns a local bapak-bapak joke when locale is id', function () {
+    app()->setLocale('id');
+
+    $joke = Joke::tellAJoke();
+    // Cek apakah output mengandung format HTML yang kita buat
+    expect($joke)->toBeString()->not->toBeEmpty();
+});
