1f57aeae7e2cc3b89773d898b5b02149467d15d7
Salman Al Majali <undermod007@gmail.com>
2026-01-28 21:31:26 +0700

    fix translation keys in joke widget for consistency

--- resources/views/widgets/joke-widget.blade.php
+++ resources/views/widgets/joke-widget.blade.php
@@ -1,15 +1,14 @@
 <x-filament::section icon="heroicon-m-megaphone">
     <x-slot name="heading">
-        {{ __('bapack-jokes::translations.widget_heading') }}
+        {{ __('jokes-widget::jokes-widget.widget_heading') }}
     </x-slot>
 
     <x-slot name="afterHeader">
-        <x-filament::icon-button 
-            icon="heroicon-m-arrow-path" 
-            wire:click="tellMeAnotherJoke" 
-            label="{{ __('bapack-jokes::translations.refresh_button') }}" 
-        />
+        <x-filament::icon-button
+            icon="heroicon-m-arrow-path"
+            wire:click="tellMeAnotherJoke"
+            label="{{ __('jokes-widget::jokes-widget.refresh_button') }}" />
     </x-slot>
     
-    {!! $joke !!}
+    <p style="font-size: 1.2rem; line-height: calc(1.5 / 1); font-style: italic; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; font-weight: 400;">“ {!! $joke !!} ”</p>
 </x-filament::section>
